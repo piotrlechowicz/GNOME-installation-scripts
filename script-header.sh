21b1c42e432c3258002311348c1ab8d44ab9502b
Piotr Lechowicz <piotr.tobiasz.lechowicz@gmail.com>
2016-01-04 23:08:39 +0100

    add checking if script is run as root:

--- script-header.sh
+++ script-header.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# check if it is run as sudo
+if [[ $(id -u) -ne 0 ]]; then
+    echo "Please run as root"
+    exit 1
+fi
+
+# function to install packages if not installed
 function install_package {
 	package_name="$1"
 	if [ $(dpkg --get-selections $package_name 2>/dev/null | grep -c "install") -eq "0" ]; then 
