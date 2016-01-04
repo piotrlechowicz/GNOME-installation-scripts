7789b2346b7b79bb5ab65e2febda557c58cfa25d
Piotr Lechowicz <piotr.tobiasz.lechowicz@gmail.com>
2016-01-04 23:35:00 +0100

    add function to checking python modules

--- script-header.sh
+++ script-header.sh
@@ -10,8 +10,24 @@ fi
 function install_package {
 	package_name="$1"
 	if [ $(dpkg --get-selections $package_name 2>/dev/null | grep -c "install") -eq "0" ]; then 
-		echo "not installed";
+		apt-get install $package_name
 	else
-		echo "package $package_name installed"
+		echo "package $package_name already installed"
 	fi
+}
+
+# check if there is python module installed
+# it returns 0 if module is not installed
+function has_python_module {
+	module="$1"
+	python <<END
+import sys
+try:
+    print "checking if python module $module is installed"
+    import $module
+except ImportError:
+    sys.exit(1)
+print "$module is installed"
+END
+	return $?
 }
